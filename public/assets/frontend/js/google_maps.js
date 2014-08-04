b58ec5e67b8af709cf9aa5ae68560707b1588144
lk1195 <lk1195@ya.ru>
2014-08-04 21:40:18 +0400

    temp commit

--- public/assets/frontend/js/google_maps.js
+++ public/assets/frontend/js/google_maps.js
@@ -1,8 +1,10 @@
 function initialize() {
 	var mapOptions = {
-		center: new google.maps.LatLng(45.4654219, 9.1859243),
+		center: new google.maps.LatLng(20.4654219, 9.1859243),
     	disableDefaultUI: true,
-		zoom: 2.5,
+    	scrollwheel: false,
+    	draggable: false,
+		zoom: 3,
 		mapTypeId: google.maps.MapTypeId.TERRAIN
 	};
 	window.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
@@ -11,6 +13,4 @@ function initialize() {
 $(document).ready(function(){
 	initialize();
 	$('#map_canvas').height( $(window).height()-50 );
-
-
 });
---
+++ public/assets/frontend/js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$('#container').css('top', '300px');
+});
