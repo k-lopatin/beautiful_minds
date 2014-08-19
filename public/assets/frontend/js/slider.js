4f88a4ab8bc3865546c516eecf3c3a87243dd6a2
Rostislav <shtanko243@gmail.com>
2014-08-19 20:53:35 +0400

    Slider 0.0.3

--- public/assets/frontend/js/slider.js
+++ public/assets/frontend/js/slider.js
@@ -3,28 +3,24 @@ $(function(){  //shorthand for $(document).ready()
     var width = $('#slidesContainer div').width();  // get the width of our div
 
     function slide(){
-
-        $('#slidesContainer').delay(1000).animate({right: '+=' + width},1000, function(){
-            // move the entire track right using the width of our div
-            // use delay so that the animation has a pause in between
-            // after the animation, we run a callback function
-
-            var first = $('#slidesContainer div:first-child');
-            // get the first div
-
-            first.remove();
-            // delete the first div
-
-            $(this).append(first);
-            // add the first div to the end of the track
-
-            $(this).css({right: '-=' + width});
-            // reposition the track
-
-            slide();
-            // start the animation again
-        });
-
+        var divarr = $('div.slide');
+        divarr[0].fadeOut();
+        divarr[1].fadeIn();
+        /*for(var i=0; i<divarr.length; i++){
+            divarr[i].fadeOut();
+        }
+        var i = 0;
+        while(true)
+        {
+            divarr[i].fadeIn(1000);
+            if(i!=0)
+                divarr[i-1].fadeOut(1000);
+            i++;
+            if(i==4)
+                i=0;
+        }*/
+       // }
+        slide();
     }
 
     slide();
